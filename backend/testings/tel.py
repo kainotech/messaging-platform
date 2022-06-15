bca16f2d5713f54279028cb334be36a162d9debb
Saranga Kumarapeli <65158515+Saranga99@users.noreply.github.com>
2022-06-15 16:35:23 +0530

    update

--- backend/testings/tel.py
+++ backend/testings/tel.py
@@ -1,8 +1,13 @@
-from telethon.sync import TelegramClient
+import telepot
 api_id = "16614431"
 api_hash = '48f640ce7e1dc5b434d2f382dddac989'
 bot_token = '5471537894:AAGQe_UHJNHqP_6UsZcGp5Ndt0DAgwbAQAE'
 # We have to manually call "start" if we want an explicit bot token
-bot = TelegramClient('testing_kaino_bot', api_id, api_hash).start(bot_token=bot_token)
+# bot = TelegramClient('testing_kaino_bot', api_id, api_hash).start(bot_token=bot_token)
 # But then we can use the client instance as usual
-bot.send_message("94701613315","hii")
+# bot.send_message(0000000,"hii")
+
+bot=telepot.Bot(bot_token)
+bot.sendMessage(1090822693,"Hi_there")
+
+# https://api.telegram.org/bot5471537894:AAGQe_UHJNHqP_6UsZcGp5Ndt0DAgwbAQAE/getUpdates
--- backend/testings/telegram.py
+++ backend/testings/telegram.py
@@ -7,13 +7,13 @@ from telethon import TelegramClient, sync, events
 
 # get your api_id, api_hash, token
 # from telegram as described above
-api_id = "16614431"
-api_hash = "48f640ce7e1dc5b434d2f382dddac989"
+api_id = '16614431'
+api_hash = '48f640ce7e1dc5b434d2f382dddac989'
 token = '5471537894:AAGQe_UHJNHqP_6UsZcGp5Ndt0DAgwbAQAE'
 message = "Working..."
-
+#32554
 # your phone number
-phone = "94701613315"
+phone = '+94701613315'
 
 # creating a telegram session and assigning
 # it to a variable client
@@ -44,7 +44,7 @@ except Exception as e:
 	
 	# there may be many error coming in while like peer
 	# error, wrong access_hash, flood_error, etc
-	print(e)
+	print(e);
 
 # disconnecting the telegram session
 client.disconnect()
